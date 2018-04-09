773bc2a8cca168d9a4da67c0f90333f1e8ec09a3
HP <HP@10.1.0.128>
2018-04-09 19:40:20 +0200

    12.03.2018

--- OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/Figury.java
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/Figury.java
@@ -22,5 +22,54 @@ public class Figury {
     public static double ObwodKola(double r){
         return 2*Math.PI*r;
     }
+    /**
+     * Metoda statyczna obliczająca obwód kwadratu
+     */
+    public static double PoleProstokąta(double a,double b){
+        return a*b;
+    }
+    /**
+     * Metoda statyczna obliczająca obwód kwadratu
+     */
+    public static double ObwodProstokąta(double a,double b){
+        return (2*a)+(2*b);
+    }
+    /**
+     * Metoda statyczna obliczająca pole kwadratu
+     */
+    public static double PoleKawadratu(double aa){
+        return aa*aa;
+    }
+    /**
+     * Metoda statyczna obliczająca obwód kwadratu
+     */
+    public static double ObwódKwadratu(double a){
+        
+        return 4*a;
+    }
+    /**
+     * Metoda statyczna obliczająca pole stożka
+     */
+    public static double PoleStożka(double r,double l){
+        return (Math.PI*(r*r))+(Math.PI*r*l);
+    }
+    /**
+     * Metoda statyczna obliczająca objetosc stożka
+     */
+    public static double VStożka(double r,double h){
+        return (1/3)*((Math.PI *(Math.pow(r, 2)))*h);
+    }
+    /**
+     * Metoda statyczna obliczająca pole walca
+     */
+    public static double PoleWalca(double r,double h){
+        return (2*(Math.PI*(Math.pow(r, 2))))+(2*Math.PI*r*h);
+    }
+    /**
+     * Metoda statyczna obliczająca v walca
+     */
+    public static double VWalca(double r,double H){
+        return Math.PI*(Math.pow(r, 2))*H;
+    }
     
 }
--- OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/SkladoweStatyczne.java
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/SkladoweStatyczne.java
@@ -7,6 +7,14 @@ public class SkladoweStatyczne {
         
         System.out.println(Figury.PoleKola(0.5));
         System.out.println(Figury.ObwodKola(0.5));
+        System.out.println(Figury.ObwódKwadratu(0.5));
+        System.out.println(Figury.PoleKawadratu(0.5));
+        System.out.println(Figury.PoleProstokąta(0.5,2));
+        System.out.println(Figury.ObwodProstokąta(0.5,2));
+        System.out.println(Figury.PoleStożka(0.5,2));
+        System.out.println(Figury.VStożka(0.5,2));
+        System.out.println(Figury.VWalca(0.5,2));
+        System.out.println(Figury.PoleWalca(0.5,2));
     }
     
 }
